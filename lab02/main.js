9c58bd4bcf6d3dbcdb17fd6b66293c300ab847e5
mikhail_surin <surin97@uab.edu>
2022-06-19 21:38:15 -0500

    finished lab2

--- lab02/main.js
+++ lab02/main.js
@@ -1,12 +1,12 @@
 function calculate() {
-    var str = document.getElementById("input").value;
+    var str = document.getElementById("input");
     try {
-        eval(str);
-        console.log(isFinite(eval(str))) 
-        if (!isFinite(str))
+        eval(str.value);
+        console.log(isFinite(eval(str.value))) 
+        if (!isFinite(eval(str.value)))
         {
             alert("Division by Zero!");
-            document.getElementById("result").innerHTML = "Error";
+            //document.getElementById("result").innerHTML = "Error";
             return
         }
     } 
@@ -15,7 +15,10 @@ function calculate() {
        alert("You must enter numbers");
     
     }
-    document.getElementById("result").innerHTML = eval(str);
+    //document.getElementById("result").innerHTML = eval(str.value);
+    str.value = str.value + " \= " + eval(str.value);
+    document.getElementById("input").focus()
+    
   }
 
 function add() {
@@ -41,3 +44,39 @@ function div() {
     orig.value = orig.value + "/";
     document.getElementById("input").focus()
 }
+
+function sqrt() {
+    var orig = document.getElementById("input");
+    try {
+        if (eval(orig.value) < 0){
+            alert("Cannot take a square root");
+        }
+        else{
+            orig.value = Math.sqrt(eval(orig.value))
+            document.getElementById("input").focus() 
+        }
+    }
+
+    catch (e){
+
+        alert("Cannot take a square root");
+    }
+}
+
+function power() {
+    var orig = document.getElementById("input");
+    var array = orig.value.split("power")
+    console.log(array)
+    try {
+            var res = Math.pow(eval(array[0]),eval(array[1]))
+            
+            orig.value = res
+            document.getElementById("input").focus() 
+        
+    }
+
+    catch (e){
+
+        alert("Cannot Exponentiate");
+    }
+  }
